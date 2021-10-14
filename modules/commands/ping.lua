741bfe7c111c4589081a332d648240711c216b5b
dowoge <tommyrodriguezdelrio@gmail.com>
2021-10-13 22:57:15 -0400

    ping.lua file cleaned up :o

--- modules/commands/ping.lua
+++ modules/commands/ping.lua
@@ -2,9 +2,5 @@ local discordia=require('discordia')
 discordia.extensions()
 local commands=require('./../commands.lua')
 commands:Add('ping',{'pong','latency'},"Returns the bot's latency (it doesnt lol)", function(message,args)
-    if #args~=0 then
-        return message:reply('why u passing args for a ping command.......?')
-    else
-        return message:reply('ping 👍')
-    end
+    return message:reply('ping 👍')
 end)
